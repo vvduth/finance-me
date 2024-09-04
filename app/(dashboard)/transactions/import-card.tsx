90cee68eff1389975b757619e92ad1b50aed3fc9
gja5899 <anh-duc.thai@teliacompany.com>
2024-09-04 13:58:00 +0300

    transaction import csv funnctionality

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -9,8 +9,11 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import ImportTable from "./import-table";
+import {format, parse} from "date-fns"
+import { boolean } from "drizzle-orm/mysql-core";
+import { convertAmountToMiliUnits } from "@/lib/utils";
 const dateFormat = "yyyy-MM-dd HH:mm:ss";
-const outoutFormat = "yyyy-MM-dd";
+const outputFormat = "yyyy-MM-dd";
 
 const requiredOptions = ["amount", "date", "payee"];
 
@@ -26,10 +29,76 @@ type Props = {
 const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
   const headers = data[0];
   const body = data.slice(1);
-
   const [selectedColumns, setSelectedColumns] = useState<SelectedColumnsState>(
     {}
   );
+  const onTableHeadSelectChange = (
+    columnIndex: number,
+    value: string | null
+  ) => {
+    setSelectedColumns((prev) => {
+      const newSelectedColumns = { ...prev };
+
+      for (const key in newSelectedColumns) {
+        if (newSelectedColumns[key] === value) {
+          newSelectedColumns[key] = null;
+        }
+      }
+
+      if (value === "skip") {
+        value = null;
+      }
+
+      newSelectedColumns[`column_${columnIndex}`] = value;
+      return newSelectedColumns;
+    });
+  };
+
+  const progress = Object.values(selectedColumns).filter(Boolean).length;
+
+  const handleContinue = () => {
+    const getColumnIndex = (column: string) => {
+      return column.split("_")[1];
+    };
+
+    const mappedData = {
+      headers: headers.map((_header, index) => {
+        const columnIndex = getColumnIndex(`column_${index}`);
+        return selectedColumns[`column_${columnIndex}`] || null;
+      }),
+      body: body
+        .map((row) => {
+          const tranformedRow = row.map((cell, index) => {
+            const columnIndex = getColumnIndex(`column_${index}`);
+            return selectedColumns[`column:_${columnIndex}`] ? cell : null;
+          });
+          return tranformedRow.every((item) => item === null)
+            ? []
+            : tranformedRow;
+        })
+        .filter((row) => row.length > 0),
+    };
+
+    const arrayOfData = mappedData.body.map((row) => {
+      return row.reduce((acc: any, cell, index) => {
+        const header = mappedData.headers[index];
+        if (header !== null) {
+          acc[header] = cell;
+        }
+        return acc;
+      }, {});
+    });
+
+    const formattedData = arrayOfData.map((item) => ({
+      ...item, 
+      amount: convertAmountToMiliUnits(parseFloat(item.amount)),
+      date: format(parse(item.date, dateFormat, new Date()), outputFormat)
+    }))
+
+    console.log(formattedData)
+    //onSubmit(formattedData)
+  };
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -40,9 +109,16 @@ const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
           <CardTitle className="text-xl line-clamp-1">
             Import transaction
           </CardTitle>
-          <div className="flex gap-x-2 items-center">
-            <Button onClick={onCancel} size={"sm"}>
+          <div className="flex flex-col lg:flex-row gap-y-2 gap-x-2 items-center">
+            <Button className="w-full lg:w-auto" onClick={onCancel} size={"sm"}>
               Cancel
+            </Button>
+            <Button
+              className="w-full lg:w-auto"
+              size={"sm"}
+              disabled={progress < requiredOptions.length}
+            >
+              Continue ({progress}/ {requiredOptions.length})
             </Button>
           </div>
         </CardHeader>
@@ -51,7 +127,7 @@ const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
             headers={headers}
             body={body}
             selectedColumns={selectedColumns}
-            onTableHeadSelectChange={() => {}}
+            onTableHeadSelectChange={onTableHeadSelectChange}
           />
         </CardContent>
       </Card>
--- app/(dashboard)/transactions/import-table.tsx
+++ app/(dashboard)/transactions/import-table.tsx
@@ -7,6 +7,7 @@ import {
     TableHeader,
     TableRow,
   } from "@/components/ui/table";
+import TableHeadSelect from './table-head-select';
 
   type Props ={
     headers: string[], 
@@ -16,7 +17,34 @@ import {
 }
 const ImportTable = ({headers, body, selectedColumns, onTableHeadSelectChange}: Props) => {
   return (
-    <div>ImportTable</div>
+    <div className='rounded-md border overflow-hidden'>
+        <Table>
+            <TableHeader className='bg-muted'>
+                <TableRow>
+                    {headers.map((_item, index)=>(
+                        <TableHead key={index}>
+                            <TableHeadSelect 
+                                columnIndex={index}
+                                selectedColumns={selectedColumns}
+                                onChange={onTableHeadSelectChange}
+                            />
+                        </TableHead>
+                    ))}
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {body.map((row: string[], index) =>(
+                    <TableRow key={index}>
+                        {row.map((cell, index) => (
+                            <TableCell key={index}>
+                                {cell}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </div>
   )
 }
 
